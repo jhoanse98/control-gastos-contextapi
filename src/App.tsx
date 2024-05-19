b579afb7c321c17d58e02b2068bc42f275528658
Sebas24 <sebastian.marin@lernit.com.mx>
2024-05-19 14:24:30 -0500

    se crea vista inicial del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import BudgetForm from "./components/BudgetForm";
+
 function App() {
-  return <h1 className="text-7xl">control de gastos</h1>;
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          {" "}
+          Planificador de gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm />
+      </div>
+    </>
+  );
 }
 
 export default App;
