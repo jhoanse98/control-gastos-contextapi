5418993bc817d7f6696b5edf7618e9552a6e2c65
Sebas24 <sebastian.marin@lernit.com.mx>
2024-06-05 11:50:18 -0500

    feat: tipando el formulario

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { categories } from "../data/categories";
 import DatePicker from "react-date-picker";
 import "react-date-picker/dist/DatePicker.css";
 import "react-calendar/dist/Calendar.css";
+import { DraftExpense } from "../types";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: "",
+    category: "",
+    date: new Date(),
+  });
+
+  const handleChangeDate = (value: Value) => {
+    setExpense({
+      ...expense,
+      date: value,
+    });
+  };
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    const isAmountField = ["amount"].includes(name);
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? Number(value) : value,
+    });
+  };
   return (
     <form className="space-y-5">
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -25,6 +50,8 @@ const ExpenseForm = () => {
           placeholder="Añade el nombre del gasto"
           className="bg-slate-100 p-2"
           name="expenseName"
+          value={expense.expenseName}
+          onChange={handleChange}
         />
       </div>
       <div className="flex flex-col gap-2">
@@ -37,13 +64,21 @@ const ExpenseForm = () => {
           placeholder="Añade la cantidad del gasto: ej. 300"
           className="bg-slate-100 p-2"
           name="amount"
+          value={expense.amount}
+          onChange={handleChange}
         />
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="amount" className="text-xl">
+        <label htmlFor="category" className="text-xl">
           Categorias:{" "}
         </label>
-        <select id="category" className="bg-slate-100 p-2" name="category">
+        <select
+          id="category"
+          className="bg-slate-100 p-2"
+          name="category"
+          value={expense.category}
+          onChange={handleChange}
+        >
           <option value="">-- Seleccione --</option>
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -53,10 +88,14 @@ const ExpenseForm = () => {
         </select>
       </div>
       <div className="flex flex-col gap-2">
-        <label htmlFor="amount" className="text-xl">
+        <label htmlFor="date" className="text-xl">
           Fecha de gasto:{" "}
         </label>
-        <DatePicker className="bg-slate-100 p-2 border-0" />
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          onChange={handleChangeDate}
+          value={expense.date}
+        />
       </div>
       <input
         type="submit"
